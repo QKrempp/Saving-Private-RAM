07cccdba888f4960041051a06933f5c207f7e678
QuentinK <qkrempp@qkp.fr>
2025-09-13 23:42:45 +0200

    feat: Added weight to CountableEntity

--- RAM.gd
+++ RAM.gd
@@ -1,23 +1,36 @@
 class_name RamOverlay extends Label
 
-const TOTAL_RAM: int = 32
+@export var total_ram: int = 32
+@export var ram_threshold_warning: float = 0.8
+@export var default_text_color: Color = Color.WEB_GRAY
 var used_ram: int = 0
+var fps: int = 60
 
 func _ready() -> void:
-	add_theme_color_override("font_color", Color.WHITE)
-	text = "Available RAM: " + str(used_ram) + " / " + str(TOTAL_RAM) + " Mo"
-
-func _on_countable_entity_spawned() -> void:
-	used_ram += 1
+	add_theme_color_override("font_color", default_text_color)
+	upate_overlay_text()
+	
+func _on_countable_entity_spawned(weight: int) -> void:
+	used_ram += weight
 	refresh_ram_counter()
 	
-func _on_countable_entity_destroyed() -> void:
-	used_ram -=1
+func _on_countable_entity_destroyed(weight: int) -> void:
+	used_ram -= weight
 	refresh_ram_counter()
 
 func refresh_ram_counter() -> void:
-	text = "Available RAM: " + str(used_ram) + " / " + str(TOTAL_RAM) + " Mo"
-	if float(used_ram) / float(TOTAL_RAM) > 0.8:
+	var ram_pct: float = float(used_ram) / float(total_ram)
+	if ram_pct > ram_threshold_warning:
+		fps = int(60.0 * (1.0 - ram_pct) / (1.0 - ram_threshold_warning))
 		add_theme_color_override("font_color", Color.RED)
-	if used_ram >= TOTAL_RAM:
+	else:
+		fps = 60
+		add_theme_color_override("font_color", default_text_color)
+	Engine.max_fps = fps
+	if used_ram >= total_ram:
 		get_tree().quit()
+	upate_overlay_text()
+
+func upate_overlay_text() -> void:
+	text = "Available RAM: " + str(used_ram) + " / " + str(total_ram) + " Mo\nFPS: " + str(fps)
+	
--- enemy.gd
+++ enemy.gd
@@ -13,10 +13,10 @@ func _physics_process(_delta: float) -> void:
 	for index in range(get_slide_collision_count()):
 		var collision = get_slide_collision(index)
 		if collision and collision.get_collider() and collision.get_collider().is_in_group("bullets"):
-				var bullet = collision.get_collider()
-				hit(bullet)
-				bullet.entity_destroyed.emit()
-				bullet.queue_free()
+			var bullet = collision.get_collider()
+			hit(bullet)
+			bullet.entity_destroyed.emit()
+			bullet.queue_free()
 
 	if velocity != Vector2.ZERO:
 		animated_zombie1_sprite.play("WalkZombie1")
@@ -27,7 +27,7 @@ func _physics_process(_delta: float) -> void:
 func hit(bullet: Bullet) -> void:
 	health -= 1
 	var inst: BloodSplatter = BLOOD_SPLATTER.instantiate()
-	inst.blood_splattered.connect(_ram._on_countable_entity_spawned.bind())
+	inst.blood_splattered.connect(_ram._on_countable_entity_spawned.bind(1))
 	inst.start(global_position, bullet.rotation)
 	get_tree().current_scene.add_child(inst)
 	if health <= 0:
