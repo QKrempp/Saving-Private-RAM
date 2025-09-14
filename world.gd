9d9ad6d4bad6700e2a8f9bedaad5e9d74a3db90b
Antoine Flichy <antoine.flichy@pearltrees.net>
2025-09-14 14:49:07 +0200

    Add hitboxes to first walls

--- world.gd
+++ world.gd
@@ -9,39 +9,6 @@ const RIGHT := 1
 const LEFT := 0
 const VOID := -1
 
-enum VOISINS {
-	HAUT	   = 1 << 0,  # 1
-	HAUT_DROITE = 1 << 1, # 2
-	DROITE	 = 1 << 2, # 4
-	BAS_DROITE  = 1 << 3, # 8
-	BAS		= 1 << 4,  # 16
-	BAS_GAUCHE  = 1 << 5, # 32
-	GAUCHE	 = 1 << 6, # 64
-	HAUT_GAUCHE = 1 << 7, # 128
-}
-
-func calculer_masque_voisins(x: int, y: int, grille: Array) -> int:
-	var masque = 0
-	# Vérifie chaque voisin et met à jour le masque
-	if x > 0 and grille[y][x-1] == 0:  # Gauche
-		masque |= VOISINS.GAUCHE
-	if x > 0 and y > 0 and grille[y-1][x-1] == 0:  # Haut-Gauche
-		masque |= VOISINS.HAUT_GAUCHE
-	if y > 0 and grille[y-1][x]:  # Haut
-		masque |= VOISINS.HAUT
-	if x < grille[0].size() - 1 and y > 0 and grille[y-1][x+1]:  # Haut-Droite
-		masque |= VOISINS.HAUT_DROITE
-	if x < grille[0].size() - 1 and grille[y][x+1]:  # Droite
-		masque |= VOISINS.DROITE
-	if x < grille[0].size() - 1 and y < grille.size() - 1 and grille[y+1][x+1]:  # Bas-Droite
-		masque |= VOISINS.BAS_DROITE
-	if y < grille.size() - 1 and grille[y+1][x]:  # Bas
-		masque |= VOISINS.BAS
-	if x > 0 and y < grille.size() - 1 and grille[y+1][x-1]:  # Bas-Gauche
-		masque |= VOISINS.BAS_GAUCHE
-	return masque
-
-
 func render(grid: Array, x: int, y: int):
 	if grid[y][x] == 1:
 		var alt := compute_orientation(grid, x, y)
@@ -60,7 +27,6 @@ func compute_orientation(grid: Array, x: int, y: int) -> Vector2i:
 	var top_left := 1
 	var bottom_right := 1
 	var bottom_left := 1
-	print(calculer_masque_voisins(x, y, grid))
 	if y > 0:
 		top = grid[y-1][x]
 		if x > 0:
