eb200cafbdce5bdd9197b3b50ae91ce66b69fe79
QuentinK <qkrempp@qkp.fr>
2025-09-14 11:06:22 +0200

    feat: Randomized timers

--- enemy_spawner.gd
+++ enemy_spawner.gd
@@ -5,6 +5,10 @@ const ENEMY: PackedScene = preload("res://Enemy.tscn")
 signal enemy_spawned
 
 @onready var _spawn_area_shape = $SpawnArea/CollisionShape2D
+@onready var _spawn_timer = $SpawnTimer
+
+func _ready() -> void:
+	_spawn_timer.wait_time = 3 + 3 * randf()
 
 func _on_spawn_timer_timeout() -> void:
 	var spawn_position = _spawn_area_shape.global_position
